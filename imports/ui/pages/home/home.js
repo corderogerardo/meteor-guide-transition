16d27fab2f19af2e0b5a9215a38b98ee1f0b7dd7
Gerardo Cordero <cordero.gerard@gmail.com>
2020-01-25 16:48:51 -0500

    Finalmente: eliminamos la carpeta imports ya que no la usamos.

--- imports/ui/pages/home/home.js
+++
@@ -1,8 +0,0 @@
-import './home.html';
-
-import '../../components/hello/hello.js';
-import '../../components/info/info.js';
-
-// Podemos importar nuestro componente aca de la misma forma que esta arriba
-
-import '../../../../client/scripts/helloWorldEager.js';
